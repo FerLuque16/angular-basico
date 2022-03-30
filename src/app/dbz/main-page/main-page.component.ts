4e9ab20423ae022661b617e8892491af2b6e8ae5
FerLuque16 <feerluque77@gmail.com>
2022-03-29 23:40:34 -0300

    Mas coigo agregado

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import { Personaje } from '../interfaces/dbz.interface';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.css']
+})
+export class MainPageComponent{
+
+  personajes: Personaje[] =[
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 8500
+    }
+  ]
+
+  nuevo: Personaje= {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  verEstado(){
+    console.log(this.personajes);
+  }
+
+  
+}
